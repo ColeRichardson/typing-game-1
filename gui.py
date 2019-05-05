c0658e54fd02f7e8177ad4985c5053bf6cdc5929
Cole <coler1999@hotmail.com>
2019-05-05 18:53:33 -0400

    worked on timing, need to find where to start timer and end timer, also need to add wpm and play again button

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import time
 
 class GUI:
 	def __init__(self):
@@ -12,12 +13,33 @@ class GUI:
 		Label2 = tk.Label(self.root, text=self.sentence, background='green', font=15, padx=20, pady=20)
 
 		test = self.root.register(self.checkEntry)
-		self.Entry1 = tk.Entry(self.root, bg='yellow', width=100, bd=3, font=20, validate='key', validatecommand=(test, '%V'))
+		self.Entry1 = tk.Entry(self.root, bg='yellow', width=100, bd=3, font=20, validate='key', validatecommand=(test, '%i'))
 
 		Label1.pack()
 		Label2.pack()
 		self.Entry1.pack()
 
 
-	def checkEntry(self, char):	
+
+	def checkEntry(self, index):
+		print(index)
+		print(self.Entry1.get())
+		if index == 0:
+			self.start_timer()
 		return True
+
+	def get_wpm(self, time):
+		time_min = time/60
+		wpm = time_min/len(self.sentence)
+
+	def start_timer(self):
+		start = time.time()
+		while self.Entry1.get() != self.sentence:
+			pass
+		end = time.time()
+		time1 = start - end
+		print(time1)
+
+	
+
+
