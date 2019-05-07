b29d6e6373e1dfdc8c8c3f9d3a53b1751ba89172
Cole <coler1999@hotmail.com>
2019-05-06 23:01:51 -0400

    finished adding wpm label, caluclating time, num_words, todo: implement prompting user for their own text file that they can import

--- gui.py
+++ gui.py
@@ -2,44 +2,100 @@ import tkinter as tk
 import time
 
 class GUI:
+
 	def __init__(self):
 		self.wpm = 0
-		self.sentence = 'this is a TEst!'
+		self.start = 0.0
+		self.end = 0.0
+		self.time1 = 0.0
+		self.check = 0
+		self.sentence = 'im pickle rick ree ree'
 		self.root = tk.Tk()
 		self.root.title("Typing Game")
 		self.root.geometry("1500x1000")
 
+		self.wpm_var = tk.StringVar()
+		self.wpm_var.set('Finish the sentence to correctly to get your wpm')
+
 		Label1 = tk.Label(self.root, text='Type this sentence', background='orange', padx=10, pady=10, font=12)
 		Label2 = tk.Label(self.root, text=self.sentence, background='green', font=15, padx=20, pady=20)
-
+		self.wpm_Label = tk.Label(self.root, textvariable=self.wpm_var, background='red', font=15, padx=20, pady=20)
 		test = self.root.register(self.checkEntry)
-		self.Entry1 = tk.Entry(self.root, bg='yellow', width=100, bd=3, font=20, validate='key', validatecommand=(test, '%i'))
+		self.Entry1 = tk.Entry(self.root, bg='yellow', width=100, bd=3, font=20, validate='key', validatecommand=(test, '%i', '%P'))
 
 		Label1.pack()
 		Label2.pack()
 		self.Entry1.pack()
+		self.wpm_Label.pack()
 
 
 
-	def checkEntry(self, index):
-		print(index)
-		print(self.Entry1.get())
-		if index == 0:
+	def checkEntry(self, index, text):
+		"""
+		checks the contents of the entry widget to see when the user starts typing
+		and when their sentence matches the required sentence.
+		:param index:
+		:param text:
+		:return:
+		"""
+		if self.check == 0:
+			self.check += 1
 			self.start_timer()
+
+		if text == self.sentence:
+			self.end_timer()
+			self.get_wpm()
+
 		return True
 
-	def get_wpm(self, time):
-		time_min = time/60
-		wpm = time_min/len(self.sentence)
-
 	def start_timer(self):
-		start = time.time()
-		while self.Entry1.get() != self.sentence:
-			pass
-		end = time.time()
-		time1 = start - end
-		print(time1)
+		"""
+		starts the timer
+		:return:
+		"""
+		self.start = time.time()
 
-	
+
+	def end_timer(self):
+		"""
+		ends the timer
+		:return:
+		"""
+		self.end = time.time()
+
+
+	def get_wpm(self):
+		"""
+		gets the time it took the user to type and uses that to
+		get the users words per minute (wpm)
+		:return:
+		"""
+		self.time1 = self.end - self.start
+		time_fact = 60/self.time1
+		self.wpm = time_fact * self.get_num_words()
+		self.update_wpm_label()
+
+	def get_num_words(self):
+		"""
+		gets the number of words in the sentence the user was asked to type
+		:return:
+		"""
+		num_words = 1
+		for space in self.sentence:
+			if space == ' ':
+				num_words += 1
+		return num_words
+
+	def update_wpm_label(self):
+		"""
+		update the wpm label to contain the users wpm
+		:return:
+		"""
+		self.wpm_var.set('Your wpm is: ' + str(int(self.wpm)))
+
+
+
+
+
 
 
